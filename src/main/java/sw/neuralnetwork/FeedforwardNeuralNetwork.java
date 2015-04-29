3afdeecc0268e671db584f8b706ad16703f05f41
Scott Wiedemann <lemmingapex@gmail.com>
2015-04-28 23:20:09 -0600

    Fixing init bug, adding basic tests, method scoping.

--- src/main/java/sw/neuralnetwork/FeedforwardNeuralNetwork.java
+++ src/main/java/sw/neuralnetwork/FeedforwardNeuralNetwork.java
@@ -1,188 +1,195 @@
 package sw.neuralnetwork;
 
+import java.io.PrintStream;
+
 /*
  * @author scott
  */
 public class FeedforwardNeuralNetwork {
 
-  // determines how quickly weights are updated and altered
-  private final static double RHO = 0.1;
+    // determines how quickly weights are updated and altered as a low pass filter
+    private final static double RHO = 0.1;
 
-  private double[] inputNeurons;
-  private double[] outputNeurons;
+    private double[] inputNeurons;
+    private double[] outputNeurons;
 
-  private double[] hiddenNeurons;
+    private double[] hiddenNeurons;
 
-  private double[][] weightsHiddenToInput;
-  private double[][] weightsOutputToHidden;
+    private double[][] weightsHiddenToInput;
+    private double[][] weightsOutputToHidden;
 
-  private final double[][] data;
+    private final double[][] data;
 
-  public FeedforwardNeuralNetwork(double[][] data) {
+    public FeedforwardNeuralNetwork(double[][] data) {
 
-    if(data.length == 0) {
-      throw new IllegalArgumentException("data is empty.");
+        if (data.length == 0) {
+            throw new IllegalArgumentException("data is empty.");
+        }
+
+        if (data[0].length == 0) {
+            throw new IllegalArgumentException("data is empty.");
+        }
+
+        this.data = data;
+
+        inputNeurons = new double[this.data[0].length];
+        outputNeurons = new double[this.data.length];
+
+        // TODO : what size should this be?
+        hiddenNeurons = new double[Math.max(Math.max(inputNeurons.length, outputNeurons.length), 2)];
+
+        weightsHiddenToInput = new double[hiddenNeurons.length][inputNeurons.length];
+        weightsOutputToHidden = new double[outputNeurons.length][hiddenNeurons.length];
+
+        for (int i = 0; i < inputNeurons.length; i++) {
+            inputNeurons[i] = 1.0;
+        }
+
+        for (int i = 0; i < hiddenNeurons.length; i++) {
+            hiddenNeurons[i] = 1.0;
+        }
+
+        for (int j = 0; j < hiddenNeurons.length; j++) {
+            for (int i = 0; i < inputNeurons.length; i++) {
+                weightsHiddenToInput[j][i] = Math.random() - 0.5;
+            }
+        }
+
+        for (int j = 0; j < outputNeurons.length; j++) {
+            for (int i = 0; i < hiddenNeurons.length; i++) {
+                weightsOutputToHidden[j][i] = Math.random() - 0.5;
+            }
+        }
     }
 
-    if(data[0].length == 0) {
-      throw new IllegalArgumentException("data is empty.");
+    private double currentMeanSquaredError(int index) {
+        if (index < 0 || index >= outputNeurons.length) {
+            throw new IllegalArgumentException("Index is not within bounds.  Must be greater than of equal to " + 0 + " and less than " + outputNeurons.length + ".");
+        }
+
+        double mse = 0.0;
+
+        for (int i = 0; i < outputNeurons.length; i++) {
+            double input = (index == i) ? 1.0 : 0.0;
+            double e = input - outputNeurons[i];
+            mse += e * e;
+        }
+
+        return mse / (double) outputNeurons.length;
     }
 
-    this.data = data;
+    private void setInputNeurons(double[] input) {
+        if (inputNeurons.length != input.length) {
+            throw new IllegalArgumentException("Input is not the right dimension.  Expected " + inputNeurons.length + ". Was " + input.length + ".");
+        }
 
-    inputNeurons = new double[this.data[0].length];
-    outputNeurons = new double[this.data.length];
-
-    // TODO : what size should this be?
-    hiddenNeurons = new double[Math.max(inputNeurons.length, outputNeurons.length)];
-
-    weightsHiddenToInput = new double[hiddenNeurons.length][inputNeurons.length];
-    weightsOutputToHidden = new double[outputNeurons.length][hiddenNeurons.length];
-
-    //double RAND_WEIGHT = Math.random() - 0.5;
-    double RAND_WEIGHT = 0.8;
-
-    for(int i = 0; i < inputNeurons.length; i++) {
-      inputNeurons[i] = 1.0;
+        for (int i = 0; i < inputNeurons.length; i++) {
+            inputNeurons[i] = input[i];
+        }
     }
 
-    for(int i = 0; i < hiddenNeurons.length; i++) {
-      hiddenNeurons[i] = 1.0;
+    private void feedForward() {
+        for (int i = 0; i < hiddenNeurons.length; i++) {
+            hiddenNeurons[i] = 0.0;
+            for (int j = 0; j < inputNeurons.length; j++) {
+                hiddenNeurons[i] += (weightsHiddenToInput[i][j] * inputNeurons[j]);
+            }
+
+            hiddenNeurons[i] = ANNMath.sigmoid(hiddenNeurons[i]);
+        }
+
+        for (int i = 0; i < outputNeurons.length; i++) {
+            outputNeurons[i] = 0.0;
+            for (int j = 0; j < hiddenNeurons.length; j++) {
+                outputNeurons[i] += (weightsOutputToHidden[i][j] * hiddenNeurons[j]);
+            }
+
+            outputNeurons[i] = ANNMath.sigmoid(outputNeurons[i]);
+        }
     }
 
-    for (int j = 0 ; j < hiddenNeurons.length; j++) {
-      for (int i = 0 ; i < inputNeurons.length; i++) {
-        weightsHiddenToInput[j][i] = RAND_WEIGHT;
-      }
+    private void printOutputNeurons(PrintStream s) {
+        for (int i = 0; i < outputNeurons.length; i++) {
+            s.println(i + " : " + outputNeurons[i]);
+        }
     }
 
-    for (int j = 0 ; j < outputNeurons.length; j++) {
-      for (int i = 0 ; i < hiddenNeurons.length; i++) {
-        weightsOutputToHidden[j][i] = RAND_WEIGHT;
-      }
-    }
-  }
+    private void backpropagate(int index) {
+        if (index < 0 || index >= outputNeurons.length) {
+            throw new IllegalArgumentException("Index is not within bounds.  Must be greater than of equal to " + 0 + " and less than " + outputNeurons.length + ".");
+        }
 
-  private double currentMeanSquaredError(int index) {
-    double mse = 0.0;
+        double[] errOutput = new double[outputNeurons.length];
+        double[] errHidden = new double[hiddenNeurons.length];
 
-    for (int i = 0; i < outputNeurons.length; i++) {
-      double input = (index == i) ? 1.0 : 0.0;
-      double e = input - outputNeurons[i];
-      mse += e * e;
-    }
+        for (int i = 0; i < outputNeurons.length; i++) {
+            double input = (index == i) ? 1.0 : 0.0;
+            errOutput[i] = (input - outputNeurons[i]) * ANNMath.sigmoidDerivative(outputNeurons[i]);
+        }
 
-    return ( mse / (double)outputNeurons.length );
-  }
+        for (int i = 0; i < hiddenNeurons.length; i++) {
+            errHidden[i] = 0.0;
 
-  public void setInputNeurons(double[] input) {
-    if(inputNeurons.length != input.length) {
-      throw new IllegalArgumentException("Input is not the right dimension.  Expected " + inputNeurons.length + ". Was " + input.length + ".");
-    }
+            // Include error contribution for all output neurons
+            for (int j = 0; j < outputNeurons.length; j++) {
+                errHidden[i] += errOutput[j] * weightsOutputToHidden[j][i];
+            }
 
-    for(int i = 0; i < inputNeurons.length; i++) {
-      inputNeurons[i] = input[i];
-    }
-  }
+            errHidden[i] *= ANNMath.sigmoidDerivative(hiddenNeurons[i]);
+        }
 
-  public void feedForward() {
-    for (int i = 0; i < hiddenNeurons.length; i++) {
-      hiddenNeurons[i] = 0.0;
-      for (int j = 0; j < inputNeurons.length; j++) {
-        hiddenNeurons[i] += (weightsHiddenToInput[i][j] * inputNeurons[j]);
-      }
+        for (int i = 0; i < outputNeurons.length; i++) {
+            for (int j = 0; j < hiddenNeurons.length; j++) {
+                weightsOutputToHidden[i][j] += RHO * errOutput[i] * hiddenNeurons[j];
+            }
+        }
 
-      hiddenNeurons[i] = ANNMath.sigmoid(hiddenNeurons[i]);
+        for (int i = 0; i < hiddenNeurons.length; i++) {
+            for (int j = 0; j < inputNeurons.length; j++) {
+                weightsHiddenToInput[i][j] += RHO * errHidden[i] * inputNeurons[j];
+            }
+        }
     }
 
-    for (int i = 0; i < outputNeurons.length; i++) {
-      outputNeurons[i] = 0.0;
-      for (int j = 0; j < hiddenNeurons.length; j++) {
-        outputNeurons[i] += (weightsOutputToHidden[i][j] * hiddenNeurons[j]);
-      }
+    public double train(double meanSquaredErrorThreshold) {
 
-      outputNeurons[i] = ANNMath.sigmoid(outputNeurons[i]);
-    }
-  }
+        meanSquaredErrorThreshold = Math.max(meanSquaredErrorThreshold, Double.MIN_VALUE);
 
-  private void backpropagate(int index) {
-    double[] errOutput = new double[outputNeurons.length];
-    double[] errHidden = new double[hiddenNeurons.length];
+        double mse;
+        int count = 0;
+        do {
+            // random input
+            int dataIndex = Math.min((int) (Math.random() * data.length), data.length - 1);
+            double[] input = data[dataIndex];
 
-    for (int i = 0; i < outputNeurons.length; i++) {
-      double input = (index == i) ? 1.0 : 0.0;
-      errOutput[i] = (input - outputNeurons[i]) * ANNMath.sigmoidDerivative(outputNeurons[i]);
-    }
+            setInputNeurons(input);
+            feedForward();
+            backpropagate(dataIndex);
+            mse = currentMeanSquaredError(dataIndex);
+            count++;
+        } while (mse > meanSquaredErrorThreshold);
 
-    for (int i = 0; i < hiddenNeurons.length; i++) {
-      errHidden[i] = 0.0;
-
-      // Include error contribution for all output neurons
-      for (int j = 0; j < outputNeurons.length; j++) {
-        errHidden[i] += errOutput[j] * weightsOutputToHidden[j][i];
-      }
-
-      errHidden[i] *= ANNMath.sigmoidDerivative(hiddenNeurons[i]);
+        return mse;
     }
 
-    for (int i = 0; i < outputNeurons.length; i++) {
-      for (int j = 0; j < hiddenNeurons.length; j++) {
-        weightsOutputToHidden[i][j] += RHO * errOutput[i] * hiddenNeurons[j];
-      }
+    public double train() {
+        return train(0.0001);
     }
 
-    for (int i = 0; i < hiddenNeurons.length; i++) {
-      for (int j = 0; j < inputNeurons.length; j++) {
-        weightsHiddenToInput[i][j] += RHO * errHidden[i] * inputNeurons[j];
-      }
+    public int classify(double[] input) {
+        setInputNeurons(input);
+        feedForward();
+
+        int bestIndex = 0;
+        double max = outputNeurons[bestIndex];
+
+        for (int i = 1; i < outputNeurons.length; i++) {
+            if (outputNeurons[i] > max) {
+                max = outputNeurons[i];
+                bestIndex = i;
+            }
+        }
+
+        return bestIndex;
     }
-  }
-
-  public double train(double meanSquaredErrorThreshold) {
-
-    meanSquaredErrorThreshold = Math.max(meanSquaredErrorThreshold, Double.MIN_VALUE);
-
-    // use each input at least once
-    for(int dataIndex = 0; dataIndex < data.length; dataIndex++) {
-      double[] input = data[dataIndex];
-
-      setInputNeurons(input);
-      feedForward();
-      backpropagate(dataIndex);
-    }
-
-    // make better predictions, by feeding more data in
-    double mse;
-    int count = 0;
-    do {
-      // random input
-      int dataIndex = Math.min((int) (Math.random() * data.length), data.length - 1);
-      double[] input = data[dataIndex];
-
-      setInputNeurons(input);
-      feedForward();
-      backpropagate(dataIndex);
-      mse = currentMeanSquaredError(dataIndex);
-      count++;
-    } while (mse > meanSquaredErrorThreshold);
-
-    return mse;
-  }
-
-  public int classify(double[] input) {
-    setInputNeurons(input);
-    feedForward();
-
-    int bestIndex = 0;
-    double max = outputNeurons[bestIndex];
-
-    for (int i = 1; i < outputNeurons.length; i++) {
-      if (outputNeurons[i] > max) {
-        max = outputNeurons[i];
-        bestIndex = i;
-      }
-    }
-
-    return bestIndex;
-  }
 }
--- src/test/java/sw/neuralnetwork/FeedforwardNeuralNetworkTest.java
+++ src/test/java/sw/neuralnetwork/FeedforwardNeuralNetworkTest.java
@@ -1,6 +1,7 @@
 package sw.neuralnetwork;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /*
@@ -8,18 +9,103 @@ import static org.junit.Assert.*;
  */
 public class FeedforwardNeuralNetworkTest {
 
-  @Test
-  public void testTraining() {
+    @Test
+    public void testSimpleClassification() {
 
-    final double[][] data = new double[][]{
-            {0,0,0,0,0},
-            {1,1,1,1,1}
-    };
+        final double[][] data = new double[][]{
+                {0, 0},
+                {1, 1}
+        };
 
-    FeedforwardNeuralNetwork feedforwardNeuralNetwork = new FeedforwardNeuralNetwork(data);
-    feedforwardNeuralNetwork.train(0.1);
+        FeedforwardNeuralNetwork feedforwardNeuralNetwork = new FeedforwardNeuralNetwork(data);
+        feedforwardNeuralNetwork.train();
 
-    assertEquals(0, feedforwardNeuralNetwork.classify(data[0]));
-    assertEquals(1, feedforwardNeuralNetwork.classify(data[1]));
-  }
+        for (int i = 0; i < data.length; i++) {
+            assertEquals(i, feedforwardNeuralNetwork.classify(data[i]));
+        }
+    }
+
+    @Test
+    public void testSimpleClassification1() {
+
+        final double[][] data = new double[][]{
+                {0, 1, 1, 1, 0, // 0
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 0},
+                {0, 0, 1, 0, 0, // 1
+                        0, 1, 1, 0, 0,
+                        0, 0, 1, 0, 0,
+                        0, 0, 1, 0, 0,
+                        0, 0, 1, 0, 0,
+                        0, 0, 1, 0, 0,
+                        0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 0, // 2
+                        1, 0, 0, 0, 1,
+                        0, 0, 0, 0, 1,
+                        0, 0, 1, 1, 0,
+                        0, 1, 0, 0, 0,
+                        1, 0, 0, 0, 0,
+                        1, 1, 1, 1, 1},
+                {0, 1, 1, 1, 0, // 3
+                        1, 0, 0, 0, 1,
+                        0, 0, 0, 0, 1,
+                        0, 0, 1, 1, 0,
+                        0, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 0},
+                {0, 0, 0, 1, 0, // 4
+                        0, 0, 1, 1, 0,
+                        0, 1, 0, 1, 0,
+                        1, 1, 1, 1, 1,
+                        0, 0, 0, 1, 0,
+                        0, 0, 0, 1, 0,
+                        0, 0, 0, 1, 0},
+                {1, 1, 1, 1, 1, // 5
+                        1, 0, 0, 0, 0,
+                        1, 0, 0, 0, 0,
+                        1, 1, 1, 1, 0,
+                        0, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 0, // 6
+                        1, 0, 0, 0, 0,
+                        1, 0, 0, 0, 0,
+                        1, 1, 1, 1, 0,
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 0},
+                {1, 1, 1, 1, 1, // 7
+                        1, 0, 0, 0, 1,
+                        0, 0, 0, 0, 1,
+                        0, 0, 0, 1, 0,
+                        0, 0, 1, 0, 0,
+                        0, 1, 0, 0, 0,
+                        0, 1, 0, 0, 0},
+                {0, 1, 1, 1, 0, // 8
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 0,
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 0, // 9
+                        1, 0, 0, 0, 1,
+                        1, 0, 0, 0, 1,
+                        0, 1, 1, 1, 1,
+                        0, 0, 0, 0, 1,
+                        0, 0, 0, 1, 0,
+                        0, 1, 1, 0, 0}
+        };
+
+        FeedforwardNeuralNetwork feedforwardNeuralNetwork = new FeedforwardNeuralNetwork(data);
+        feedforwardNeuralNetwork.train();
+
+        for (int i = 0; i < data.length; i++) {
+            assertEquals(i, feedforwardNeuralNetwork.classify(data[i]));
+        }
+    }
 }
